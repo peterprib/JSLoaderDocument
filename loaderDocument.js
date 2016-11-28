1ec13a92d8dd7bad4386ab8f523f1e9961385137
peterprib <peter.prib@gmail.com>
2016-11-28 12:06:26 +1100

    Update loaderDocument.js

--- loaderDocument.js
+++ loaderDocument.js
@@ -62,3 +62,12 @@ LoaderDocument.prototype.history = {
 		,css:{}
 	};
 var loaderDocument = new LoaderDocument();
+
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+define(function(require) {
+    //The value returned from the function is
+    //used as the module export visible to Node.
+    return LoaderDocument;
+});
